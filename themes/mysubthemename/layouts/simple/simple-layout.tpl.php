d267ddb23b0ebb778135190c68f8768840542114
praveen89j <praveen081@hotmail.com>
2015-09-22 17:55:25 +0530

    theme chnages done

--- themes/mysubthemename/layouts/simple/simple-layout.tpl.php
+++ themes/mysubthemename/layouts/simple/simple-layout.tpl.php
@@ -24,13 +24,12 @@ jQuery(document).ready(function(){
 	  <?php } ?>
  
     </div>
-      <?php print render($page['branding']); ?>
+	<div class="mobile_menu"><span>Hoofdrubrieken</span></div>
+    <div class="expand_menu">
+		<?php print render($page['header']); ?>
+	</div>
     <!---->
-
   </header>
- <div class="mobile_menu">Expand Menu</div>
-    <div class="expand_menu"><?php print render($page['header']); ?></div>
-    <?php print render($page['navigation']); ?>
  
 <div class="clear"></div>
   <div class="l-main">
@@ -50,33 +49,17 @@ jQuery(document).ready(function(){
         <ul class="action-links"><?php print render($action_links); ?></ul>
       <?php endif; ?>
       <?php print render($page['content']); ?>
-	  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
-	   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
       <?php //print $feed_icons; ?>
     </div>
 
-    <?php //print render($page['sidebar_first']); ?>
-    <?php //print render($page['sidebar_second']); ?>
   </div>
-
+<div class="content_bottom"></div>
   <footer class="l-footer" role="contentinfo">
-    <?php //print render($page['footer']); ?>
-	<div class="colum-1"> 
-<h4>Quick Links</h4>	
-	    <ul>
-         	<li><a href="#">Links</a></li>
-			<li><a href="#">Links</a></li>
-			<li><a href="#">Links</a></li>
-			<li><a href="#">Links</a></li>
-		</ul>
-	</div>
-		<div class="colum-2"> 
-		<h4>Fast connect </h4>	
-				<p>At full width all three columns will be displayed side by side. As the page is resized the third column will collapse under the first and second.</p>
-	</div>
-		<div class="colum-3"><h4>Contact us</h4>		
-			<p>At full width all three columns will be displayed side by side. As the page is resized the third column will collapse under the first and second. </p>	
- 
+    <div class="footer_top"><?php print render($page['footer']); ?></div>
+	<div class="clear"></div>
+    <div class="footer_bottom">
+     <img src="sites/default/files/oranjebalk.gif">	 	<div class="clear"></div>
+	 &copy; All Rights Reserved.
 	</div>
   </footer>
 </div>
